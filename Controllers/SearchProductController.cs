19c6fd639c5bf8e8e7e1455e60a2afd41b2abfcf
doddypranatha <d.pranatha@tds-group.com>
2020-11-08 10:10:53 +0800

    Finishing profile area

--- Controllers/SearchProductController.cs
+++ Controllers/SearchProductController.cs
@@ -8,5 +8,7 @@ namespace HeroApp.Controllers
         {
             return View();
         }
+
+
     }
 }
--- wwwroot/js/Profile.js
+++ wwwroot/js/Profile.js
@@ -13,12 +13,23 @@ Profile.prototype = {
                 e.preventDefault();
                 var form = $(this).closest('form');
 
-                var data = JSON.parse(form.serializeArray());
+                var data = JSON.stringify(getFormData(form));
 
                 localStorage.setItem('userProfile', data);
 
                 window.location = form.data("redirect-url");
             });
+
+        function getFormData($form){
+            var unindexed_array = $form.serializeArray();
+            var indexed_array = {};
+
+            $.map(unindexed_array, function (n, i) {
+                indexed_array[n['name']] = n['value'];
+            });
+
+            return indexed_array;
+        }
 
     }
 };
